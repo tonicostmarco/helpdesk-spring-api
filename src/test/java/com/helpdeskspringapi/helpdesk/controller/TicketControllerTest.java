f089016c55a73b989db92812ba1d2ad131169301
tonicostmarco <marcoricardo61b@gmail.com>
2026-03-12 20:17:35 -0300

    test(ticket): start implementing TicketController unit tests

--- src/test/java/com/helpdeskspringapi/helpdesk/controller/TicketControllerTest.java
+++ src/test/java/com/helpdeskspringapi/helpdesk/controller/TicketControllerTest.java
@@ -1,4 +1,160 @@
 package com.helpdeskspringapi.helpdesk.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.helpdeskspringapi.helpdesk.dtos.ticket.TicketDTO;
+import com.helpdeskspringapi.helpdesk.dtos.ticket.TicketInputDTO;
+import com.helpdeskspringapi.helpdesk.dtos.ticket.TicketMinDTO;
+import com.helpdeskspringapi.helpdesk.dtos.ticket.TicketPatchDTO;
+import com.helpdeskspringapi.helpdesk.entities.Category;
+import com.helpdeskspringapi.helpdesk.entities.Ticket;
+import com.helpdeskspringapi.helpdesk.entities.User;
+import com.helpdeskspringapi.helpdesk.exceptions.ResourceNotFoundException;
+import com.helpdeskspringapi.helpdesk.factory.CategoryFactory;
+import com.helpdeskspringapi.helpdesk.factory.TicketFactory;
+import com.helpdeskspringapi.helpdesk.services.TicketService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+import java.util.Set;
+
+@WebMvcTest(TicketController.class)
 public class TicketControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private TicketService service;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long dependentId;
+    private Set<Long> ids;
+    private Set<Long> wrongIds;
+    private String expectedTitle;
+    private String nonExistingTitle;
+    private String expectedCategory;
+    private String nonExistingCategory;
+
+    private Ticket ticket;
+    private TicketDTO ticketDTO;
+    private TicketMinDTO minDTO;
+    private TicketInputDTO inputDTO;
+    private TicketPatchDTO patchDTO;
+    private List<TicketMinDTO> listMinDTO;
+    private List<Category> categories;
+
+    private PageImpl<Ticket> page;
+    private PageImpl<TicketMinDTO> pageMin;
+    private Pageable pageable;
+
+    @BeforeEach
+    void setUp() {
+
+        existingId = 1L;
+        nonExistingId = 100L;
+        expectedTitle = "Internet cai do nada";
+        nonExistingTitle = "nonExistingTitle";
+        expectedCategory = "DNS";
+        nonExistingCategory = "nonExistingCategory";
+
+        ticket = TicketFactory.createTicket();
+        ticketDTO = TicketFactory.createTicketDTO();
+        minDTO = TicketFactory.createTicketMinDTO();
+        inputDTO = TicketFactory.createTicketInputDTO();
+        patchDTO = TicketFactory.createTicketPatchDTO();
+
+        listMinDTO = List.of(minDTO);
+
+        page = new PageImpl<>(List.of(ticket));
+        pageMin = new PageImpl<>(List.of(minDTO));
+        pageable = PageRequest.of(0, 10);
+
+        when(service.findById(existingId)).thenReturn(minDTO);
+        when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+
+        when(service.insert(inputDTO)).thenReturn(minDTO);
+
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void findAllShouldReturnPagedTicket() throws Exception {
+
+        ResultActions result = mockMvc.perform(get("/tickets", pageable)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void findByIdShouldReturnTicketWhenIdExists() throws Exception {
+
+        ResultActions result = mockMvc.perform(get("/tickets/{id}", existingId)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").exists());
+        result.andExpect(jsonPath("$.title").exists());
+        result.andExpect(jsonPath("$.client").exists());
+        result.andExpect(jsonPath("$.status").exists());
+        result.andExpect(jsonPath("$.priority").exists());
+        result.andExpect(jsonPath("$.createdAt").exists());
+
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void findByIdShouldReturnNothingWhenTicketIdExists() throws Exception {
+
+        ResultActions result = mockMvc.perform(get("/tickets/{id}", nonExistingId)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void shouldInsertTicketWhenCorrectData() throws Exception {
+
+        String jsonBody = objectMapper.writeValueAsString(inputDTO);
+
+        ResultActions result = mockMvc.perform(post("/tickets").with(csrf())
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isCreated());
+        result.andExpect(jsonPath("$.id").exists());
+        result.andExpect(jsonPath("$.title").exists());
+        result.andExpect(jsonPath("$.client").exists());
+        result.andExpect(jsonPath("$.status").exists());
+        result.andExpect(jsonPath("$.priority").exists());
+        result.andExpect(jsonPath("$.createdAt").exists());
+    }
+
 }
